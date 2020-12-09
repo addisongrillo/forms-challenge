ad2e0dbb27b507d3f2ad3624f8041e94bd40988a
Addison Grillo <addisongrillo@gmail.com>
2020-12-08 22:48:35 -0500

    basicFormat

--- src/components/form/form.js
+++ src/components/form/form.js
@@ -1,26 +1,114 @@
-import React from 'react'
+import {React, useState} from 'react'
+import './form.css';
+import { TextField, Grid, Button } from '@material-ui/core';
+import DateFnsUtils from '@date-io/date-fns';
+import 'date-fns';
+import {
+    MuiPickersUtilsProvider,
+    KeyboardTimePicker,
+    KeyboardDatePicker,
+  } from '@material-ui/pickers';
+
 
 
 function Form() {
+  //hooks
+  const [selectedTicket, setSelectedTicket] = useState('');
+  const [selectedDescription, setSelectedDescription] = useState('');
+  const [selectedDate, setSelectedDate] = useState(new Date(Date.now()));
+  const [selectedStartTime, setSelectedStartTime] = useState(new Date('2020-01-01T12:00:00'));
+  const [selectedEndTime, setSelectedEndTime] = useState(new Date('2020-01-01T12:00:00'));
+
+  //onChange Functions
+  const handleTicketChange = (event) => {
+    setSelectedTicket(event.target.value);
+  };
+  const handleDescriptionChange = (event) => {
+    setSelectedDescription(event.target.value);
+  };
+  const handleDateChange = (date) => {
+    setSelectedDate(date);
+  };
+  const handleStartTimeChange = (time) => {
+    setSelectedStartTime(time);
+  };
+  const handleEndTimeChange = (time) => {
+    setSelectedEndTime(time);
+  };
   return (
     <div className="Form">
       <form data-testid='form' className='form-group'>
-        <label htmlFor='ticket'>Ticket Name:</label>
-        <input data-testid='ticket-input' id='ticket' type='text'/>
 
-        <label htmlFor='description'>Description:</label>
-        <input data-testid='description-input' id='description'/>
+        
 
-        <label htmlFor='Date'>Date:</label>
-        <input data-testid='date-input' id='date' type='text'/>
+        <MuiPickersUtilsProvider utils={DateFnsUtils}>
+            <Grid id="formGrid" container >
+                <TextField 
+                value={selectedTicket} 
+                onChange={handleTicketChange}
+                className="formField" 
+                data-testid='ticket-input' 
+                id="ticket" 
+                label="Ticket Name" 
+                variant="outlined" />
 
-        <label htmlFor='startTime'>Start Time:</label>
-        <input data-testid='startTime-input' id='startTime' type='text'/>
+                <TextField 
+                value={selectedDescription}
+                onChange={handleDescriptionChange}
+                multiline
+                rowsMax={4}
+                className="formField" 
+                data-testid='description-input' 
+                id="description" 
+                label="Description" v
+                ariant="outlined" />
 
-        <label htmlFor='endTime'>End Time:</label>
-        <input data-testid='endTime-input' id='endTime' type='text'/>
+                <KeyboardDatePicker
+                className="formField"
+                data-testid='date-input'
+                disableToolbar
+                variant="inline"
+                format="MM/dd/yyyy"
+                margin="normal"
+                id="Date"
+                label="Date"
+                value={selectedDate}
+                onChange={handleDateChange}
+                KeyboardButtonProps={{
+                    'aria-label': 'change date',
+                }}
+                />
 
-        <button>Submit</button>
+                <KeyboardTimePicker
+                className="formField"
+                data-testid='startTime-input'
+                margin="normal"
+                id="startTime"
+                label="Start Time"
+                value={selectedStartTime}
+                onChange={handleStartTimeChange}
+                KeyboardButtonProps={{
+                    'aria-label': 'change time',
+                }}
+                />
+
+                <KeyboardTimePicker
+                className="formField"
+                data-testid='endTime-input'
+                margin="normal"
+                id="endTime"
+                label="End Time"
+                value={selectedEndTime}
+                onChange={handleEndTimeChange}
+                KeyboardButtonProps={{
+                    'aria-label': 'change time',
+                }}
+                />
+                <Button variant="contained" color="primary">Submit</Button>
+         </Grid>
+        </MuiPickersUtilsProvider>
+
+        
       </form>
     </div>
   );
